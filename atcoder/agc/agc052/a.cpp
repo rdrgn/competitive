38a44508a5283457ad5b3db592c9299cb4d4d004
remin <rdrgn@users.noreply.github.com>
2021-03-07 23:43:38 +0900

    add agc052

---
+++ atcoder/agc/agc052/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    string s1, s2, s3;
+    cin >> n >> s1 >> s2 >> s3;
+    cout << string(n, '0') + string(n, '1') + "0" << endl;
+  }
+}
